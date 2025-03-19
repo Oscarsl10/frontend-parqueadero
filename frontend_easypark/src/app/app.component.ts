eaf665ecf099f2328b6d7e3bade266eab8ef8e1d
karencaicedo1907 <kjcaicedo-2023a@corhuila.edu.co>
2025-03-19 10:00:28 -0500

    🚗 Frontend easyPark

---
+++ frontend_easypark/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],  
+  template: `<router-outlet></router-outlet>`,  
+  providers: [] 
+})
+export class AppComponent {}
